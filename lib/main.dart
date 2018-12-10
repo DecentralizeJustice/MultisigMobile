625f17419946200174587772d5ac82c8c9fbdd31
Don Gunn <dgoon@Dons-MacBook-Pro.local>
2018-12-10 13:02:53 -0800

    VsCode Test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Helfdafa'),
+          child: Text('Gdd'),
         ),
       ),
     );
